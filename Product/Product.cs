4d53dfd3297dee9ef976f942d718045ed7dc51db
catalinacris <catalinacucos14@yahoo.com>
2022-05-01 06:01:13 +0200

    Add project files.

---
+++ Product/Product.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    enum Categories
+    {
+        Dairy, 
+        Fruit, 
+        Beverages, 
+        Bread
+    }
+
+    class Product
+    {
+        private double weight;
+        private string name;
+        DateTime productionDate;
+        private string producer;
+        public Categories categories;
+
+
+        public Product(double weight, string name, DateTime productionDate, string producer, Categories categories)
+        {
+            this.weight = weight;
+            this.name = name;
+            this.productionDate = productionDate;
+            this.producer = producer;
+            this.categories = categories;
+            
+        }
+
+        
+
+        public DateTime ExpirationDate(Product product)
+        {
+            if(product.categories == Categories.Dairy)
+            {
+                return productionDate.AddDays(7);
+            }
+
+            else if(product.categories == Categories.Fruit)
+            {
+                return productionDate.AddDays(2);
+            }
+
+            else if (product.categories == Categories.Beverages)
+            {
+                return productionDate.AddYears(3);
+            }
+            else
+            {
+                return productionDate.AddDays(4);
+            }
+            
+        }
+    }
+}
---
+++ Product/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product(3.5, "chifla", new DateTime(2022,01,04), "panifcom", Categories.Bread);
+            product.ExpirationDate(product);
+            Console.WriteLine(product.ExpirationDate(product));
+            
+
+
+        }
+    }
+}
+
+
